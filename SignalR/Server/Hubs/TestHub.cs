4a4f3833a7f8f7cbb2aad79198a11f3c5d12389d
Dragan Pepic <d.pepic@hotmail.com>
2020-03-19 20:39:34 +0100

    Add project files.

---
+++ SignalR/Server/Hubs/TestHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR.Server.Hubs
+{
+	public class TestHub : Hub
+	{
+		public async Task ProbnaMetoda()
+		{
+			await Clients.Caller.SendAsync("KlijentMetoda", "Poruka od servera");
+		}
+	}
+}
